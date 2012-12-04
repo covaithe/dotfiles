79b0a3c48198d00bbbc3475aeaf7aeccd768a753
Greg Fleming <covaithe@gmail.com>
2012-12-03 19:26:20 -0500

    drop brew dir

--- brew/completion.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if which brew >/dev/null 2>&1; then
-  if [ -f `brew --prefix`/Library/Contributions/brew_bash_completion.sh ]; then
-    . `brew --prefix`/Library/Contributions/brew_bash_completion.sh
-  fi
-fi
